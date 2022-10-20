3e54b59d7cdac656fb2fef21763eb0b087d805c6
NatanEspino <75447232+NatanEspino@users.noreply.github.com>
2022-10-20 14:45:36 -0700

    Update fragmentShader.glsl

--- fragmentShader.glsl
+++ fragmentShader.glsl
@@ -1,8 +1,14 @@
 #version 330 core
-out vec4 fragColor;
-in vec3 ourColor;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTexCoord;
+
+out vec2 TexCoord;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-    fragColor = vec4(ourColor, 1.0f);
+	gl_Position = projection * view * model * vec4(aPos, 1.0f);
+	TexCoord = aTexCoord;
 }

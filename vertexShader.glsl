4d771fa477672f51539adfe2718bdc7f86254fa7
NatanEspino <75447232+NatanEspino@users.noreply.github.com>
2022-10-20 14:46:27 -0700

    Update vertexShader.glsl

--- vertexShader.glsl
+++ vertexShader.glsl
@@ -1,14 +1,13 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec2 aTexCoord;
+out vec4 FragColor;
 
-out vec2 TexCoord;
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-	gl_Position = projection * view * model * vec4(aPos, 1.0f);
-	TexCoord = aTexCoord;
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }

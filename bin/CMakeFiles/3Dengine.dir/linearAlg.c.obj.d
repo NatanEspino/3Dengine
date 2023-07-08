f5cc58503533c0c1b0286275ba3a12c9117ffc99
Natan Espino <unconfigured@null.spigotmc.org>
2023-07-08 10:14:17 -0700

    new file:   images/chiaki.png 	modified:   src/main.c

---
+++ bin/CMakeFiles/3Dengine.dir/linearAlg.c.obj.d
@@ -0,0 +1,15 @@
+CMakeFiles/3Dengine.dir/linearAlg.c.obj: \
+ C:\Users\Natan\Desktop\3Dengine\src\linearAlg.c \
+ C:/Users/Natan/Desktop/3Dengine/include/linearAlg/linearAlg.h \
+ C:/msys64/mingw64/include/string.h C:/msys64/mingw64/include/crtdefs.h \
+ C:/msys64/mingw64/include/corecrt.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/vadefs.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/include/sec_api/string_s.h \
+ C:/msys64/mingw64/include/math.h C:/msys64/mingw64/include/stdio.h \
+ C:/msys64/mingw64/include/corecrt_stdio_config.h \
+ C:/msys64/mingw64/include/_mingw_off_t.h \
+ C:/msys64/mingw64/include/swprintf.inl \
+ C:/msys64/mingw64/include/sec_api/stdio_s.h
--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 
 
 GLuint loadShader(const char *path, GLenum type){
-    // Read shader code from file path
+    // read shader code from file path
     FILE *f;
     f = fopen(path, "r");
 
@@ -38,13 +38,13 @@ GLuint loadShader(const char *path, GLenum type){
     shaderCode[i] = 0;
     fclose(f);
 
-    // Create and compile shader
+    // create and compile shader
     GLuint Shader = glCreateShader(type);
     glShaderSource(Shader, 1, (const GLchar**)&shaderCode, NULL);
     glCompileShader(Shader);
     free(shaderCode);
     
-    // Check for compilation errors
+    // check for compilation errors
     GLint success;
     GLint maxLength = 512;
     GLchar log[maxLength];
@@ -61,50 +61,52 @@ GLuint loadShader(const char *path, GLenum type){
     return Shader;
 }
 
+
+
 int main(){
     
     glfwInit();
     
-    // Set OpenGL version and profile
+    // set OpenGL version and profile
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     
-    // Create GLFW window
+    // create GLFW window
     const unsigned int SCR_WIDTH = 800;
     const unsigned int SCR_HEIGHT = 600;
 	const float aspectRatio = (float)SCR_WIDTH / (float)SCR_HEIGHT;
 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    // Terminate window if it fails to initialize
+    // terminate window if it fails to initialize
     if (window == NULL) {
         printf("Failed to create GLFW window\n");
         glfwTerminate();
         return -1;
     }
 
-    // Add window to the current context
+    // add window to the current context
     glfwMakeContextCurrent(window);
 
-    // Initialize glad
+    // initialize glad
     gladLoadGL();
 
     glEnable(GL_DEPTH_TEST);
 
-    // Specify OpenGL viewport
+    // specify OpenGL viewport
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
     
-    //Create vertex and fragment shaders
+    // create vertex and fragment shaders
     GLuint vertexShader = loadShader("../shaders/vertexShader.vs", GL_VERTEX_SHADER);
     GLuint fragmentShader = loadShader("../shaders/fragmentShader.fs", GL_FRAGMENT_SHADER);
 
-    // Attach shaders to shaderProgram
+    // attach shaders to shaderProgram
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
-    // Check if program linked successfully
+    // check if program linked successfully
     GLint success;
     GLint maxLength = 512;
     char log[maxLength];
@@ -115,7 +117,7 @@ int main(){
         printf("Error while linking shader:\n%s", log);
     }
 
-    // Cleanup
+    // cleanup
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 	
@@ -177,7 +179,7 @@ int main(){
 		{{-1.3f, 1.0f, -1.5f}}
 	};
 	
-    //Create vertex array object, vertex and element buffer objects
+    // create vertex array object, vertex and element buffer objects
     GLuint VAO, VBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -233,7 +235,7 @@ int main(){
     // load image, create texture and generate mipmaps
     data = stbi_load("../images/chiaki.png", &width, &height, &nrChannels, 0);
     if (data) {
-        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+        // note that the png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
@@ -246,17 +248,19 @@ int main(){
     glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
     glUniform1i(glGetUniformLocation(shaderProgram, "texture2"), 1);
 	
+    // rotation axis
 	vec3 v = {{1.0f, 0.3f, 0.5f}};
+	
 
     mat4 model = IDENTITY_MATRIX;
     unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
     glUniformMatrix4fv(modelLoc, 1, GL_TRUE, (const GLfloat *)&model.m);
     
-	float radius = 7.0f;
-	float time = glfwGetTime();
-	vec3 cameraPosition = {{radius * cosf(time), 0.0f, radius * sinf(time)}};
-	vec3 cameraTarget = {{0.0f, 0.0f, 0.0f}};
-    mat4 view = lookAt(cameraPosition, cameraTarget, jHat);
+	vec3 cameraPosition = {{0.0f, 0.0f, 3.0f}};
+	vec3 cameraFront = {{0.0f, 0.0f, -1.0f}};
+    vec3 camerUp = {{0.0f, 1.0f, 0.0f}};
+	
+    mat4 view = lookAt(cameraPosition, addVec(cameraPosition, cameraFront), jHat);
 	
     unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
     glUniformMatrix4fv(viewLoc, 1, GL_TRUE, (const GLfloat *)&view.m);
@@ -275,15 +279,12 @@ int main(){
         glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        time = glfwGetTime();
+        float time = glfwGetTime();
 		
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
 		
-		cameraPosition.x = radius * sinf(time);
-		cameraPosition.z = radius * cosf(time);
-		
-		view = lookAt(cameraPosition, cameraTarget, jHat);
+		view = lookAt(cameraPosition, addVec(cameraPosition, cameraFront), jHat);
 		glUniformMatrix4fv(viewLoc, 1, GL_TRUE, (const GLfloat *)&view.m);
 		
 		for(unsigned int i = 0; i < 10; i++){
@@ -308,3 +309,4 @@ int main(){
     glfwTerminate();
     return 0;
 }
+
